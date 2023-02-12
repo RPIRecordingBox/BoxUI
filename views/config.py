eaf8d69c8b74a54923b8239059130b8530e32bd5
Bowserinator <bowserinator+box@gmail.com>
2023-02-12 14:59:24 -0500

    Add button to open wifi config

--- views/config.py
+++ views/config.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QLabel, QPushButton, QListWidget, QVBoxLayout, QProgressBar
 from src.generic_view import GenericView
+import subprocess
 
 """
 Other config options (not including updating)
@@ -10,8 +11,20 @@ class ConfigView(GenericView):
         
         layout = QVBoxLayout()
 
+        wifi_btn = QPushButton("Open WiFi Settings")
+        wifi_btn.setMinimumSize(120, 70)
+        wifi_btn.setObjectName("wifiBtn")
+        wifi_btn.clicked.connect(self.open_wifi)
+        self.wifi_btn = wifi_btn
+        layout.addWidget(wifi_btn)
 
         self.setLayout(layout)
+
+    def open_wifi(self):
+        """
+        Open linux settings
+        """
+        subprocess.Popen(["gnome-control-center"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
     def on_open(self):
         pass
